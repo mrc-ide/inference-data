4494a6210d530edeb23a682d24baa157494c6d53
rtesra <70330391+rtesra@users.noreply.github.com>
2022-12-05 15:33:55 +0200

    Update cmr_areas.R

--- src/cmr_data_areas/cmr_areas.R
+++ src/cmr_data_areas/cmr_areas.R
@@ -1,30 +1,44 @@
+#' ## Canmeroon (CMR)
+#' Source: UNAIDS
+#' Levels:
+#'   * 1: Région Sanitaire (12)
+#'   * 2: District Sanitaire  (197)
+#' Spectrum: National (level 0)
+#' EPP: National (level 0)
+#' EPP Urban/Rural:
+#' PEPFAR PSNU:
+
+## sf v 1.0.0 update changes to use s2 spherical geometry as default
+## This creates issues for DHS coordinate data extraction scripts
+## Revert back to planar geometry
+sf::sf_use_s2(FALSE)
 dir.create("check")
 
 #' Authenticate SharePoint login
 sharepoint <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
 
 #' Read in files from SharePoint
-
 naomi_raw_path <- "sites/HIVInferenceGroup-WP/Shared%20Documents/Data/naomi-raw/"
+
 
 #' Read in files from SharePoint
 urls <- list( humdata = "CMR/2020-01-06/cmr_admbnda_inc_20180104_shp.zip",
               pepfar = "CMR/2019-25-06/Cameroon_HealthDistricts12012016.zip",
               dist = "CMR/2020-01-06/CameroonHierarchie.xlsx",
-              raw = "CMR/2020-01-18/cmr_areas.geojson.zip"
+              raw = "CMR/2020-01-18/cmr_areas.geojson.zip", 
+              boundaries_2023 = "CMR/2022-11-30%20190%20to%20197%20districts%20(WHO)/Cameroon%20197,%20from%20WHO%202022%2012.zip", 
+              districts_2023 = "CMR/2022-11-30%20190%20to%20197%20districts%20(WHO)/2022_11_30_cmr_hierarachy_wide.xlsx"
 ) %>%
   lapply(function(x) file.path(naomi_raw_path, x)) %>%
   lapply(URLencode)
 
 files <- lapply(urls, sharepoint$download)
 
-## sf v 1.0.0 update changes to use s2 spherical geometry as default
-## This creates issues for DHS coordinate data extraction scripts
-## Revert back to planar geometry
-sf::sf_use_s2(FALSE)
 
 humdata <- read_sf_zip_list(files$humdata) %>% bind_rows()
 pepfar <- read_sf_zip(files$pepfar)
+
+new <- read_sf_zip(files$dist_2022)
 
 #' Health districts
 dist <- readxl::read_excel(files$dist, 1) %>%
@@ -117,7 +131,7 @@ cmr <- cmr_wide2 %>%
 
 pryr::object_size(cmr)
 
-cmr_areas <- cmr %>%
+cmr_areas_2022 <- cmr %>%
   left_join(select(st_drop_geometry(raw), area_id, area_sort_order)) %>%
   mutate(center = sf::st_point_on_surface(geometry),
          center_x = sf::st_coordinates(center)[,1],
@@ -131,6 +145,71 @@ cmr_areas <- cmr %>%
          display = TRUE) %>%
   select(names(raw)) %>%
   arrange(area_level, area_sort_order)
+
+
+# Read in new 2023 boundaries
+district_names_2023 <- read_xlsx(files$districts_2023) %>%
+  mutate(area_name = `District sanitaire\r\n(In DHIS, New)`, 
+         area_name = area_name %>% sub("District ", "", .))
+
+# Map geomtery to area names from DHIS
+district_boundaries_2023 <- read_sf_zip(files$boundaries_2023) %>%
+  select(area_name = District_S) %>%
+  mutate(area_name = recode(area_name, 
+                            "Bétaré Oya" = "Betare Oya",    
+                            "Biyem-Assi" = "Biyem Assi",      
+                            "Cité des palmiers" = "Cite Des Palmiers",
+                            "Elig-Mfomo" = "Elig Mfomo",       
+                            "Eyumojock" = "Eyumodjock",      
+                            "Garoua 1" = "Garoua I",        
+                            "Garoua 2" = "Garoua II",       
+                            "Garoua Boulaï" = "Garoua Boulai", 
+                            "Gueré" = "Guere",            
+                            "Kumba" = "Kumba South",           
+                            "Kumba North" = "Kumba-North",     
+                            "Makari" = "Makary",         
+                            "Mvog Ada" = "Mvog-Ada",       
+                            "Ndélélé" = "Ndelele",         
+                            "New-Bell"  = "New Bell",       
+                            "Ngaoundéré Rural" = "Ngaoundere Rural",
+                            "Ngaoundéré Urbain" = "Ngaoundere Urbain",
+                            "Saa" = "Sa'a",             
+                            "Tignère" = "Tignere",        
+                            "Tokombéré" = "Tokombere",       
+                            "Zoétele"  = "Zoetele")) %>%
+  left_join(district_names_2023 %>% select(area_id = area_id3, area_name)) %>%
+  st_as_sf()
+
+
+# Compare new boundaries to old boundaries
+district_boundaries_2022 <- cmr_areas_2022 %>% filter(area_level == 3) %>% st_as_sf()
+
+ggplot() +
+  geom_sf(data = district_boundaries_2022, colour = "black", fill = NA) +
+  geom_sf(data = district_boundaries_2023, colour = "red", fill = NA)
+
+
+                            
+                            
+                            
+                            
+                            
+
+
+district_boundaries_2023 %>% filter(is.na(area_id))
+
+
+new_districts <- filter(district_names_2023, is.na(area_name3))
+
+
+
+overlaps <- st_overlaps(districts_2022, new_districts)
+
+
+
+
+
+
 
 cmr_area_hierarchy <- cmr_areas %>%
   st_set_geometry(NULL) %>%
