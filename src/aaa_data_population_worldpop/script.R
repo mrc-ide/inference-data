fa70c81be52bbb2ee27a7d9ef273faa9fb6b3140
osymandius <bolistevens@gmail.com>
2023-07-12 14:38:55 +0100

    Update data population for new areas dependency

--- src/aaa_data_population_worldpop/script.R
+++ src/aaa_data_population_worldpop/script.R
@@ -1,4 +1,4 @@
-areas <- read_area_merged("depends/naomi_areas.geojson") %>%
+areas <- read_sf("depends/naomi_areas.geojson") %>%
   st_make_valid()
 
 pop <- naomi_extract_worldpop(areas, iso3, years = c(2000, 2005, 2010, 2015, 2020))
