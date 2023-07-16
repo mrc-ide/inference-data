eefd5f63c23e10b892f689b6595b87a47fe4fb59
osymandius <bolistevens@gmail.com>
2023-07-16 11:45:29 +0100

    Pull through areas for ERI and SSD with no naomi output

--- src/aaa_pull_naomi_estimates/script.R
+++ src/aaa_pull_naomi_estimates/script.R
@@ -5,7 +5,6 @@
 #' Authenticate SharePoint login
 sharepoint <- spud::sharepoint$new("https://imperiallondon.sharepoint.com/")
 
-
 if(version == "2022") {folder <- "2022 naomi preliminary"}
 if(version == "2023") {folder <- "2023 naomi final"}
 
@@ -16,32 +15,42 @@ outputs <- folder_path %>%
   lapply(function(x) x$files()$name) %>%
   unlist()
 
-naomi_outputs <- outputs[grepl(".zip", outputs)]
-
-# if(iso3 == "SWZ"){
-#   country_output <- naomi_outputs[grepl("ESW", naomi_outputs)]
-# } else {
-  country_output <- naomi_outputs[grepl(iso3, naomi_outputs)]
-# }
-
-# Read output zip from Sharepoint
-url <- URLencode(file.path("sites/HIVInferenceGroup-WP/", folder_path, country_output))
-file <- sharepoint$download(url)
-
-# Read in naomi output zip
-out <- read_output_package(file)
-iso <- out$fit$model_options$area_scope
-
-# Stop if iso3 from output file does not match iso3 from task
-stopifnot(iso == iso3)
-
-# Add iso3 task to areas file
-areas <- out$meta_area
-areas$iso3 <- iso
-
-# Save out naomi output zip
 dir.create("outputs")
-naomi::save_output_package(out, "naomi_output", "outputs", overwrite = TRUE)
+
+if(!grep(iso3, c("SSD", "ERI"))) {
+  
+  naomi_outputs <- outputs[grepl(".zip", outputs)]
+  country_output <- naomi_outputs[grepl(iso3, naomi_outputs)]
+  
+  # Read output zip from Sharepoint
+  url <- URLencode(file.path("sites/HIVInferenceGroup-WP/", folder_path, country_output))
+  file <- sharepoint$download(url)
+  
+  # Read in naomi output zip
+  out <- read_output_package(file)
+  iso <- out$fit$model_options$area_scope
+  
+  # Stop if iso3 from output file does not match iso3 from task
+  stopifnot(iso == iso3)
+  
+  # Add iso3 task to areas file
+  areas <- out$meta_area
+  areas$iso3 <- iso
+  
+  naomi::save_output_package(out, "naomi_output", "outputs", overwrite = TRUE)
+  
+} else {
+  
+  areas_path <- outputs[grepl(tolower(iso3), outputs)]
+  url <- URLencode(file.path("sites/HIVInferenceGroup-WP/", folder_path, areas_path))
+  file <- sharepoint$download(url)
+  areas <- read_sf(file)
+  areas$iso3 <- iso3
+  
+  naomi_out <- data.frame()
+  write_csv(naomi_out, paste0(tempdir(), "/empty_df.csv"))
+  zip::zipr("outputs/naomi_output.zip", paste0(tempdir(), "/empty_df.csv"))
+}
 
 # Save areas file
 st_write(areas, "outputs/naomi_areas.geojson", delete_dsn = TRUE)
